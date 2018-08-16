d125c05a5fd80d18a16d6df995e832a5ff546698
Hugo Sykes <hugo.sykes@infinityworks.com>
2018-08-16 10:27:28 +0100

    Adds condition to check if entity is directory

--- git_pull.sh
+++ git_pull.sh
@@ -5,9 +5,11 @@ list=`ls $dir`
 
 for i in $list
 do
-  cd $dir/$i
-  echo $i
-  git pull
-  git fetch --tags
-  cd $dir
+if [ -d "${i}" ] ; then
+    cd $dir/$i
+    echo $i
+    git pull
+    git fetch --tags
+    cd $dir
+fi
 done
